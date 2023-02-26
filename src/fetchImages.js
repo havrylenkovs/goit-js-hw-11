a8dd40258cb494b3b99c50781996707e5bd577be
Vladyslav Havrylenko <110973854+havrylenkovs@users.noreply.github.com>
2023-02-26 10:49:19 +0200

    fix after advice

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = 'https://pixabay.com/api/?key=33797710-c43b349e33488e785e99b8ec8&';
 async function fetchImages(query, pageNumber) {
   const fetch = await axios.get(
-    `${url}q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNumber}`
+    `${url}q=${query}&image_type=photo&orientation=horizontal&safesearch=false&per_page=40&page=${pageNumber}`
   );
   return fetch;
 }
